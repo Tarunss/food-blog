181be62760a71ddcc1f48d85f2300a608bc23347
tsohal1 <tsohal@ucsc.edu>
2023-10-16 12:59:31 -0700

    adding all files to git

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Message from './Message'
+function App() {
+  return <div><Message></Message></div>
+}
+export default App;
---
+++ src/Message.jsx
@@ -0,0 +1,5 @@
+function Message() {
+    // JSX: Javascript XML - will be compiled into javascript code under the hood
+    return <h1>Hello World</h1>;
+}
+export default Message;
