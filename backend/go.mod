fdeefc13b85ceaaede7fcbcece75cd267a7a4402
tsohal1 <tsohal@ucsc.edu>
2023-10-18 11:15:24 -0700

    added basic echo server functionality

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/Tarunss/food-blog
 
 go 1.21.3
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+// Defining an Upgrader Struct
+// Lets us define a Read and Write Buffer Size
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// We can also check the origin of our connection with CheckOrigin, which allows us to
+	// Make connections from our react server
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// Define a Reader function that will listen for connections on that particular web socket
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		//write message out in our main
+		fmt.Println(string(p))
+		//Write message out in web socket
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+// Define our web socket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	//listen indefinitely for new messages coming in
+	//through our web socket connection
+	reader(ws)
+}
+func setUpRoutes() {
+	//Sets up default route , or (/)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "SimpleServer")
+	})
+	http.HandleFunc("/ws", serveWs)
+}
 func main() {
+	setUpRoutes()
+	http.ListenAndServe(":8080", nil)
 	fmt.Println("Hello World")
 }
