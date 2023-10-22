0e676848f373613db5daf6ee96b0857a6c9ff667
tsohal1 <tsohal@ucsc.edu>
2023-10-22 12:20:41 -0700

    Updated backend, need to test at home

--- backend/controller/controller.go
+++ backend/controller/controller.go
@@ -3,10 +3,15 @@ package controller
 //This package is meant to be a controller to our database, and initializes our connection.
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/Tarunss/food-blog/model"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -38,11 +43,122 @@ func init() {
 	fmt.Println("Colletion reference is ready")
 }
 
-// MongoDB helper method
+// MongoDB helper methods
+// Insert One Post
 func insertOnePost(post model.BlogPost) {
 	inserted, err := collection.InsertOne(context.Background(), post)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted 1 movie in DB with ID:", inserted.InsertedID)
+}
+
+// Update One Post
+func updateOnePost(postID string, body string) {
+	id, err := primitive.ObjectIDFromHex(postID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"body": body}}
+
+	res, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("modified count: ", res.ModifiedCount)
+}
+
+// Delete 1 record
+
+func deleteOnePost(postID string) {
+	id, err := primitive.ObjectIDFromHex(postID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{"_id": id}
+	res, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("deleted count:", res.DeletedCount)
+}
+
+// Delete all posts
+func deleteAllPosts() int64 {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Amount of posts deleted:", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+}
+
+// get All movies
+func getAllPosts() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var posts []primitive.M
+	for cur.Next(context.Background()) {
+		var post bson.M
+		err := cur.Decode(&post)
+		if err != nil {
+			log.Fatal(err)
+		}
+		posts = append(posts, post)
+	}
+	defer cur.Close(context.Background())
+	return posts
+}
+
+// Actual controllers - file
+// get all posts
+func GetAllPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	allPosts := getAllPosts()
+	json.NewEncoder(w).Encode(allPosts)
+}
+
+// Create Post
+
+func InsertOnePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var post model.BlogPost
+	_ = json.NewDecoder(r.Body).Decode(&post)
+	insertOnePost(post)
+	json.NewEncoder(w).Encode(post)
+}
+
+// Update Post
+func UpdateOnePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
+	params := mux.Vars(r)
+	updateOnePost(params["id"], params["body"])
+	encoder := json.NewEncoder(w)
+	encoder.Encode(params["id"])
+	encoder.Encode(params["body"])
+
+}
+
+// Delete a post
+func DeleteOnePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+	params := mux.Vars(r)
+	deleteOnePost(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+// delete All posts
+func DeleteAllPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
+
+	count := deleteAllPosts()
+	json.NewEncoder(w).Encode(count)
 }
