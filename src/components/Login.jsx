dfcf1e5e40577c118d49c243feee35e1f2df12e1
tsohal1 <tsohal@ucsc.edu>
2024-01-29 18:01:18 -0800

    added basic token functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ async function loginUser(credentials) {
         },
         body: JSON.stringify(credentials)
     })
-        .then(data => data.json).then(console.log(data.json))
+        .then(data => data.json)
 }
 
 
